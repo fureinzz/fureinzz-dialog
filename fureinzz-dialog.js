cf578a00f208809b2d21226cea37f3d3be0f83e9
Maxim Kopylov <31179849+fureinzz@users.noreply.github.com>
2020-06-10 13:03:59 +0300

    Added event listeners and comments

--- fureinzz-dialog.js
+++ fureinzz-dialog.js
@@ -2,7 +2,7 @@ import {LitElement, html} from 'lit-element'
 import {focusManager} from './dialog-focus-manager'
 import '@fureinzz/fureinzz-backdrop'
 
-class DialogElement extends LitElement {
+export class DialogElement extends LitElement {
     constructor() {
         super()
 
@@ -14,7 +14,7 @@ class DialogElement extends LitElement {
         this._canceled = null
         this._indexOfTab = -1
         this._backdrop = document.createElement('fureinzz-backdrop')
-
+        
         this._captureKey = this._captureKey.bind(this)
         this._captureClick = this._captureClick.bind(this)
         this._captureFocus = this._captureFocus.bind(this)
@@ -22,14 +22,61 @@ class DialogElement extends LitElement {
     }
     static get properties() {
         return {
-            _hasAnimation: {type: Boolean},
-            _canceled: {type: Boolean},
-            _indexOfTab: {type: Number},
-            _backdrop: {type: HTMLElement},
+            /** 
+            * Set opened to true to show the dialog component and to false to hide it.
+            * @type {!boolean}
+            * @public
+            */ 
             opened: {type: Boolean,  attribute: 'opened', reflect: true},
+
+            /** 
+            * If true, the `backdrop` element will not be added to the DOM when the dialog opens 
+            * @type {!boolean}
+            * @public
+            */ 
             noBackdrop: {type: Boolean,  attribute: 'no-backdrop', reflect: true},
+            
+            /** 
+            * If true the dialog will be closed when you press `Esc`
+            * @type {!boolean}
+            * @public
+            */ 
             closeOnEsc: {type: Boolean,  attribute: 'close-on-esc', reflect: true},
+            
+            /** 
+            * If true the dialog will be closed when you click outside the element
+            * @type {!boolean}
+            * @public
+            */ 
             closeOnOutsideClick: {type: Boolean,  attribute: 'close-on-outside-click', reflect: true},
+            
+            /** 
+            * Does the component have animation or not
+            * @type {?boolean}
+            * @private
+            */
+            _hasAnimation: {type: Boolean},
+            
+            /** 
+            * Reason for closing the dialog
+            * @type {?boolean}
+            * @private
+            */ 
+            _canceled: {type: Boolean},
+            
+            /** 
+            * Index of the element that is located in the dialog and has `tabindex` > 0 
+            * @type {number}
+            * @private
+            */ 
+            _indexOfTab: {type: Number},
+
+            /** 
+            * The overlay layer 
+            * @type {!HTMLElement}
+            * @private
+            */  
+            _backdrop: {type: HTMLElement}
         }
     }
     render() {
@@ -44,8 +91,10 @@ class DialogElement extends LitElement {
                     transform: translate(-50%, -50%)
                 }
             </style>
-            <slot></slot>`
+            <slot></slot>
+        `
     }
+
     open() { 
         this.opened = true 
     }
@@ -64,6 +113,69 @@ class DialogElement extends LitElement {
 
         this.dispatchEvent( new CustomEvent('state-changed', {detail: {canceled: this._canceled}}))
     }
+
+    /** 
+     * Focus-trap opens if the `Tab` or `Tab + Shift` key is pressed
+     * @protected
+     * @param   {!Event} event
+     * @returns {void}
+     **/ 
+    _onTab(event) {
+        event.preventDefault()
+        
+        const {shiftKey} = event
+
+        //  All elements that have `tabindex` >= 0
+        //  tabbableNodes = [{element: HTMLElement, tabIndex: Number}, ...]
+        const tabbableNodes = focusManager.getTabbableNodes(this)
+
+        // Tab + Shift
+        if(shiftKey){
+            this._indexOfTab--
+            if(this._indexOfTab < 0) this._indexOfTab = tabbableNodes.length - 1
+        } 
+        // Tab
+        else {
+            this._indexOfTab++
+            if(this._indexOfTab >= tabbableNodes.length) this._indexOfTab = 0
+        }
+
+        tabbableNodes[this._indexOfTab].element.focus()
+    }
+
+    /** 
+     * Close the dialog when the `Esc` key is pressed
+     * @protected 
+     **/ 
+    _onEsc() {
+        if(this.closeOnEsc) {
+            this.cancel()
+        }
+    }
+
+    /** 
+     * Recursive search for elements with the `cancel-button` and `confirm-button` attributes
+     * @protected 
+     * @param   {!HTMLElement} target
+     * @returns {void}
+     **/ 
+    _searchButton(target) {
+        if(target === this) return false
+        else {
+            if(target.hasAttribute('confirm-button') || target.hasAttribute('cancel-button')) {
+                target.hasAttribute('cancel-button') 
+                    ? this.cancel()
+                    : this.confirm()
+            }
+            else this._searchButton(target.parentNode)
+        }
+    }
+
+    /** 
+     * If true then dialog have a animation
+     * @protected 
+     * @returns {boolean}
+     **/ 
     _checkAnimation() {
         const {animationDuration} = this.style
         
@@ -79,25 +191,6 @@ class DialogElement extends LitElement {
         if(this.opened === false) this.style.display = 'none'
     }
 
-    _openBackdrop() {
-        document.body.appendChild(this._backdrop)
-        this._backdrop.open()
-    }
-    _closeBackdrop() {
-        this._backdrop.close()
-    }
-    updated(changedProperties) {
-        changedProperties.forEach((oldValue, property) => {
-            switch (property) {
-                case 'opened':
-                    this.openedChanged()
-                    break;
-                case 'noBackdrop':
-                    this.noBackdropChanged()
-                    break;
-            }
-        });
-    }
     _captureClick(event) {
         const {target} = event
 
@@ -107,7 +200,7 @@ class DialogElement extends LitElement {
         } 
         else this._searchButton(target) 
         
-    }  
+    }    
     _captureKey(event) {
         const {key} = event
         
@@ -133,17 +226,87 @@ class DialogElement extends LitElement {
         // Reset focus if the user clicked outside of the focused element
         if(relatedTarget === null) this._indexOfTab = -1
     }
-    noBackdropChanged() {
-        if(this.opened) {
-            this.noBackdrop ? this._closeBackdrop() : this._openBackdrop()
+    
+    // Backdrop
+        /** 
+         * Adds a backdrop to the DOM
+         * @protected
+         **/ 
+        _openBackdrop() {
+            document.body.appendChild(this._backdrop)
+            this._backdrop.open()
         }
-    }
-    connectedCallback(){
-        super.connectedCallback()
-        
-        this.setAttribute('role', 'dialog')
-        this.addEventListener('animationend', this._animationEnd)
-    }
+
+        /** 
+         * Removing dackdrop
+         * @protected 
+         **/ 
+        _closeBackdrop() {
+            this._backdrop.close()
+        }
+
+    // Observer's of properties 
+        openedChanged() {
+            if(!this.noBackdrop) {
+                this.opened ? this._openBackdrop() : this._closeBackdrop()
+            }
+
+            this._hasAnimation = this._checkAnimation()
+
+            if(this.opened) {
+                this.style.display = ''
+
+                this.addEventListener('blur', this._captureBlur, true)
+                this.addEventListener('focus', this._captureFocus, true)
+                document.addEventListener('click', this._captureClick, true)
+                document.addEventListener('keydown', this._captureKey, true)
+            } else {
+                if(!this._hasAnimation) this.style.display = 'none'
+
+                this.removeEventListener('focus', this._captureBlur, true)
+                this.removeEventListener('focus', this._captureFocus, true)
+                document.removeEventListener('click', this._captureClick, true)
+                document.removeEventListener('keydown', this._captureKey, true)
+            }
+
+            document.body.style.overflow = this.opened ? 'hidden' : ''
+            this.setAttribute('aria-hidden', !this.opened)
+        }
+        noBackdropChanged() {
+            if(this.opened) {
+                this.noBackdrop ? this._closeBackdrop() : this._openBackdrop()
+            }
+        }
+
+    // Lifecycle methods
+        updated(changedProperties) {
+            changedProperties.forEach((oldValue, property) => {
+                switch (property) {
+                    case 'opened':
+                        this.openedChanged()
+                        break;
+                    case 'noBackdrop':
+                        this.noBackdropChanged()
+                        break;
+                }
+            });
+        }
+        connectedCallback(){
+            super.connectedCallback()
+            
+            this.setAttribute('role', 'dialog')
+            this.addEventListener('animationend', this._animationEnd)
+        }
+        disconnectedCallback() {
+            super.disconnectedCallback()
+
+            // To remove all event listeners when the component is removed
+                this.removeEventListener('transitionend', this._animationEnd)
+                this.removeEventListener('focus', this._captureBlur, true)
+                this.removeEventListener('focus', this._captureFocus, true)
+                document.removeEventListener('click', this._captureClick, true)
+                document.removeEventListener('keydown', this._captureKey, true)
+        }
 }
 
 customElements.define('fureinzz-dialog', DialogElement)
