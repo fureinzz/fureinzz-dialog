218aed39ca98cf0273275bd61cb5b19aeff1dd3a
Maxim Kopylov <31179849+fureinzz@users.noreply.github.com>
2020-06-10 12:07:43 +0300

    Added basic properties

--- fureinzz-dialog.js
+++ fureinzz-dialog.js
@@ -2,7 +2,14 @@ import {LitElement, html} from 'lit-element'
 import '@fureinzz/fureinzz-backdrop'
 
 class DialogElement extends LitElement {
-
+    static get properties() {
+        return {
+            opened: {type: Boolean,  attribute: 'opened', reflect: true},
+            noBackdrop: {type: Boolean,  attribute: 'no-backdrop', reflect: true},
+            closeOnEsc: {type: Boolean,  attribute: 'close-on-esc', reflect: true},
+            closeOnOutsideClick: {type: Boolean,  attribute: 'close-on-outside-click', reflect: true},
+        }
+    }
 }
 
 customElements.define('fureinzz-dialog', DialogElement)
