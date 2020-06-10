af071e1f9218a11d27a7fc39a860dcfcee09b747
Maxim Kopylov <31179849+fureinzz@users.noreply.github.com>
2020-06-10 12:50:52 +0300

    `isFocusable` method

--- dialog-focus-manager.js
+++ dialog-focus-manager.js
@@ -15,6 +15,15 @@ class nodeFocusManager {
 
         return false
     }
+    isFocusable(element) {
+        const focusableElements = matchesSelector.call(element, 'button, textarea, input, select, object')
+        const tabIndex = element.tabIndex >= 0
+
+        
+        return focusableElements || tabIndex
+            ?  matchesSelector.call(element, ':not([disabled])')
+            :  false 
+    }
 }
 
 export const focusManager = new nodeFocusManager()
